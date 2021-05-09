6be10093b0dbf0563f3c1b21be7d726022eda828
xRave110 <xRave110@protonmail.com>
2021-05-09 18:36:35 +0200

    Added new features, improvements and example inputs/outputs

---
+++ README.md
@@ -0,0 +1,10 @@
+# Token Holders Checker
+
+Steps to run:
+1. Install necessary librares using **pip3 install -r requirements.txt**
+2. Add environment variable representing your infura ID (must have account in **infura.io**) export INFURA_ID={your id}
+3. Download csv files (with holders adresses) from **etherscan.io**
+4. Put all csv files into the folder **etherscanExports**
+5. Run **tokenHoldersChecker.py**
+6. Outputs will be in folder **tokeData**
+7. Run **charts.py** in order to get spreadsheet **Token_Total_Balances** which shows the balances for all queried ERC20 tokens
---
+++ configPkg.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 15:59:49 2021
+
+@author: anything
+"""
+import os
+
+# Params
+first_holder = 50
+default_last_holder = 3050
+percentage_to_consider = 15
+
+# Constants
+INFURA_ID = os.getenv('INFURA_ID')
+
+infura_url = 'https://mainnet.infura.io/v3/' + INFURA_ID
+ethscan_exports_path = os.getcwd()+'/etherscanExports/'
+token_data_path = os.getcwd()+'/tokenData/'
+dict_of_conv = {
+    '1': 'wei',
+    '1000': 'kwei',
+    '1000000': 'mwei' ,
+    '1000000000': 'gwei',
+    '1000000000000': 'micro' ,
+    '1000000000000000': 'milliether' ,
+    '1000000000000000000': 'ether' ,
+    '1000000000000000000000': 'kether',
+    '1000000000000000000000000': 'mether' ,
+    '1000000000000000000000000000': 'gether' ,
+    '1000000000000000000000000000000': 'tether' 
+    }
